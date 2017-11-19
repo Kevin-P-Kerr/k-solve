9d936e6ba98824cbc8baf21e4a7fa32a24e6e0bd
Kevin <kkerr87@gmail.com>
2017-11-19 15:58:39 -0500

    very basic

--- sat.c
+++ sat.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAXTOKEN 2000
+#define LPAREN 0
+#define RPAREN 1
+#define VAR 2
+#define LBRAK 3
+#define RBRAK 4
+
+struct Token {
+  int deleted;
+  int type;
+  char value;
+};
+
+struct Token* tokenize() {
+  char c;
+  int i =0;
+  struct Token* tokens = malloc(sizeof(struct Token)*MAXTOKEN);
+  while ((c=getchar())!=EOF) {
+    struct Token* t = &tokens[i];
+    t->deleted = 0;
+    if (c == '(') {
+      t->type = LPAREN;
+    }
+    else if (c == ')') {
+      t->type = RPAREN;
+    }
+    else if (c == '[') {
+      t->type = LBRAK;
+    }
+    else if (c == ']') {
+      t->type = RBRAK;
+    }
+    else {
+      t->type = VAR;
+      // for now, vars are 1 char
+      t->value = c;
+    }
+    i++;
+  }
+}
 
 int main() {
-  fprintf(stderr,"hello,world");
+  tokenize();
 }
