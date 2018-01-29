47eb286e3e8415d5c1d620f3da50340450d1bf3c
Kevin Kerr <kkerr@triplelift.com>
2018-01-29 11:32:32 -0500

    add encoder

--- notes/encode.js
+++ notes/encode.js
@@ -1,5 +1,6 @@
+var READFLAG = false;
 var fs = require('fs');
-var plato = fs.readFileSync("./lsa.txt").toString();
+var notes;
 
 var encode = function (str, key) {
     var ce = "base64";
@@ -32,22 +33,15 @@ var decode = function (encoded,key) {
     return encode(encoded,key);
 };
 
-//var morty = new Buffer("Arrays start at 1").toString("base64");
-//var rick = new Buffer("Oh no").toString("base64");
-//console.error(plato.length);
+if (READFLAG) {
+    notes = fs.readFileSync("./encrypted.txt").toString();
+    notes = new Buffer(notes,"base64").toString("base64");
+    notes = new Buffer(decode(notes,key),"base64").toString("utf8");
+    fs.writeFileSync("./notes.txt",notes);
+}
+else {
+    notes = fs.readFileSync("./notes.txt").toString();
+    notes = encode(notes,key);
+    fs.writeFileSync("./encrypted.txt", notes);
+}
 
-var morty = new Buffer(plato).toString("base64");
-var rick = new Buffer("kkerr").toString("base64");
-//console.log(new Buffer(rick,"base64").toString());
-//var s = encode(morty,rick);
-//console.log(s);
-//console.log(new Buffer(decode(s,rick),"base64").toString("utf8"));
-/*
-var henry = new Buffer("hcohen").toString("base64");
-var message = new Buffer("eq7}dW#6h|Vwe}i2","base64").toString("base64");
-console.log(new Buffer(decode(message,henry),"base64").toString());
-*/
-var s = fs.readFileSync("./secret.txt").toString();
-console.log(s);
-var ss = new Buffer(s,"base64").toString("base64");
-console.log(new Buffer(decode(ss,rick),"base64").toString("utf8"));
